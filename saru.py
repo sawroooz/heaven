8d9ce4d5ffe7ab5ee6785b6fb7dc2aaae1417702
sawroooz <sarojsigdel883@gmail.com>
2025-05-27 21:20:12 +0545

    class

--- saru.py
+++ saru.py
@@ -1,2 +1,13 @@
-x=100
-print(x+2)
+class juni:
+    def __init__(self,jun,tara):
+        self.x=jun
+        self.y=tara
+        self.homw()
+    def homw(self):
+        temp=self.x
+        self.x=self.y
+        self.y=temp 
+    def get_values(self):
+        return self.x,self.y    
+x=juni(1,2)  
+print(x.get_values())      
